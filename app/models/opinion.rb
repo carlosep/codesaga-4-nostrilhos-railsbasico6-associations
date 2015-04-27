0d977782141050d78c94323bf3a4580b1e210165
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-04-27 16:12:13 -0300

    Add opinion model spec
    * Add option model spec
    * Add opnion model

---
+++ app/models/opinion.rb
@@ -0,0 +1,3 @@
+class Opinion < ActiveRecord::Base
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150428030546) do
+
+  create_table "opinions", force: :cascade do |t|
+    t.string "body"
+  end
 
 end
