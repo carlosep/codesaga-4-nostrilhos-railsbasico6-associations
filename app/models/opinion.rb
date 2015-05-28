598d89701116a09c407a98878d787490d886f13c
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-05-28 17:20:31 -0300

    Add some validations

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,8 @@
 class Opinion < ActiveRecord::Base
-
+  validates :body, presence: true
+  validates :body, length: { maximum: 150, message: 'is too long' }
+  validates :body, format: {
+    without: /Petralha|Dilma|Tucanos/,
+    message: 'Contains political words'
+  }
 end
