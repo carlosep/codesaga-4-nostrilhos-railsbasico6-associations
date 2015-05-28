15fa234facc458f5ce147d733a105ac859496b1c
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-05-28 16:57:25 -0300

    Add some validations - specs

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -8,4 +8,28 @@ describe Opinion do
     expect(opinion).to be_persisted
     expect(opinion.body).to eq body
   end
+
+  it 'should not allow political words' do
+    body = "Petralha Tucanos Dilma"
+    opinion = Opinion.new(body: body)
+
+    expect(opinion).to_not be_valid
+    expect(opinion.errors[:body]).to include "Contains political words"
+  end
+
+  it 'is invalid when has no body' do
+    body = ''
+    opinion = Opinion.new(body: body)
+
+    expect(opinion).to_not be_valid
+    expect(opinion.errors[:body]).to include 'can\'t be blank'
+  end
+
+  it 'is invalid when has more than 150 caracters' do
+    body = 'a' * 151
+    opinion = Opinion.new(body: body)
+
+    expect(opinion).to_not be_valid
+    expect(opinion.errors[:body]).to include 'is too long'
+  end
 end
